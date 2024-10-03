aa56685a2e26b4875f7e19f10ff8030b8f9d1f51
poojithayellakari <113297408+poojithayellakari@users.noreply.github.com>
2024-10-03 14:14:56 +0530

    03/04/2024

--- api_v2/urls.py
+++ api_v2/urls.py
@@ -3,43 +3,49 @@ from . import views
 urlpatterns = [
     path("check/",views.CheckAPI.as_view()),
     path('api/register/',views.RegisterUser.as_view()),
-    path("aws_configure/", views.AWSConfigure.as_view(),name='configuration'),
+    path('api/login/', views.LoginView.as_view(),name='login'),
+    path('account-and-credentials/create/', views.AWSAccountAndCredentialManagerView.as_view(), name='account-and-credentials-create'),
+    path('account-details/', views.AccountDetailsListView.as_view(), name='account-details-list'),
     path("aws/get_details/", views.AWSAccountDetails.as_view(), name = 'aws-account-details'),
+    path('api/get_users/', views.UserListView.as_view(), name='user-list'),
+    path('api/delete_user/<str:email>/', views.UserDeleteView.as_view(), name='user-delete'),
+
+    path('api/all-services-available/',views.AvailableServices.as_view(),name="available-services"),
+
     path("aws/ec2-details/", views.AWSEC2Details.as_view(), name = 'aws-recources-details'),
     path("aws/s3-details/", views.AWSS3Details.as_view(), name = 'aws-recources-s3-details'),
     path("aws/vpc-details/", views.AWSVpcDetails.as_view(), name = 'aws-recources-s3-details'),
     path("aws/Document_DB_Details/",views.Document_DB_Details.as_view(),name='aws-document-db-details'),
     path("aws/SNS_Details/",views.SNS_Details.as_view(),name='aws-SNS_Details'),
-    path("aws/all/cost/", views.AWSAllServiceCost.as_view(), name = 'aws-recources-s3-details'),
-    path('api/all-services-available/',views.AvailableServices.as_view(),name="available-services"),
     path('api/ses-service-details/',views.SES_Details.as_view(),name="ses-details"),
- 
-    path('api/instance-usage-type/',views.Ec2_instance_usage_type.as_view(),name="ec2-instance-usage-type"),
-    path('api/get_users/', views.UserListView.as_view(), name='user-list'),
-    path('api/delete_user/<str:email>/', views.UserDeleteView.as_view(), name='user-delete'),
+
     #path("aws/configure/signout/",views.SignOutView.as_view(),name='aws-configure-signout'),
-    path('api/login/', views.LoginView.as_view(),name='login'),
+
     path('api/ec2_memory_data/',views.EC2_Memory_utilization.as_view(),name='ec2_memory_data'),#1 time limit
-    path('api/cost-data/',views.FetchAWSCostView.as_view(),name='cost-data'),#2 time
     path('api/rds-data/', views.RDSData.as_view(), name='rds_data'),#3 time
+    path('api/s3-detail-data/', views.Get_S3_Data.as_view(), name='s3-detail-data'),#5
+    path('api/vpc-data/',views.Get_VPCData.as_view(), name='VPC-detail-data'),#8 
+    path('api/ebs-data/',views.Get_EBS_Data.as_view(),name='ebs-detail-data'),#11 time
     path('api/secrets-data/', views.Secrets_data.as_view(), name='secrets-data'),#4 
-    path('api/s3-detail-data/', views.Get_S3_Data.as_view(), name='s3-detail-data'),#5 
     path('api/ecr-detail-data/', views.Get_ECR_Data.as_view(), name='ecr-detail-data'),#6
     path('api/lambda-metrics/', views.LambdaMetricsView.as_view(), name='lambda_metrics-data'),#7 time 
-    path('api/vpc-data/',views.Get_VPCData.as_view(), name='VPC-detail-data'),#8 
     path('api/ecs-data/',views.Get_ECS_Data.as_view(),name='ecs-detailed-data'),#9 time
+    path('api/eip-data/',views.Get_Elastic_Ip.as_view(),name='eip-detail-data'),#13
     path('api/loadbalancer-data/',views.Get_load_balancer_Data.as_view(),name='loadbalancer-detailed-data'),#10 time
     path('api/ebs-data/',views.Get_EBS_Data.as_view(),name='ebs-detail-data'),#11 time
     path('api/waf-acl-data/',views.Get_WAF_Data.as_view(),name='waf-detail-data'),#12 time
-    path('api/eip-data/',views.Get_Elastic_Ip.as_view(),name='eip-detail-data'),#13
-    path('api/detail-cost-data/', views.GetTotalBill.as_view(), name='detail-cost-data'),#Bar Chart#14
-    path('api/send_email/',views.Send_cost_Email.as_view(),name='send-mail'),#15 #Email
     path('api/api-gateway-data/',views.Get_APIGateway.as_view(),name='api-gateway'),#16 time
     path('api/snapshot_data/',views.Get_Snapshot_Data.as_view(),name='snapshot_data'),#17
-    path('api/get_detailed_usage_data/',views.Get_Detailed_usage_Data.as_view(),name='detailed_usage_data'),
     path('api/eks-data/',views.EKS_Data.as_view(),name='eks-data'),
+    path('api/send_email/',views.Send_cost_Email.as_view(),name='send-mail'),#15 #Email
+    path('api/get_detailed_usage_data/',views.Get_Detailed_usage_Data.as_view(),name='detailed_usage_data'),
     
-
+    path('api/detail-cost-data/', views.GetTotalBill.as_view(), name='detail-cost-data'),#Bar Chart#14
+    
+    path('api/cost-data/',views.FetchAWSCostView.as_view(),name='cost-data'),
+    path("aws/all/cost/", views.AWSAllServiceCost.as_view(), name = 'aws-recources-s3-details'),
+ 
+    path('api/instance-usage-type/',views.Ec2_instance_usage_type.as_view(),name="ec2-instance-usage-type"),
     # Graphs
 
     #path('api/EC2_instance_graphical_data/',views.EC2_Memory_utilization_Graph.as_view(),name='graph_data'),
@@ -87,6 +93,12 @@ urlpatterns = [
     path('api/vpc_recommendation/',views.VPCRecommendation.as_view(),name='vpc-recommendation'),
     path('api/ebs_recommendation/',views.EBSRecommendation.as_view(),name='ebs-recommendation'),
     path('api/lambda_recommendation/',views.LambdaMetricsAnalyzer.as_view(),name='Lambda-recommendation'),
+    path('api/ec2reccom/',views.EC2RecommendationNew.as_view(),name="ec2"),
+    
+    path('api/service_category/',views.Service_Categorization.as_view(),name="category"),
+    path('api/ec2_unused_resource/',views.EC2_UnUsedRecources.as_view(),name='unused'),
+
+    path('api/security-groups/ports/',views.SecurityGroupPortsView.as_view(),name="ports"),
 
 ]
 
